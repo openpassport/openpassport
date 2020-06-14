49da4caf83819e7817dd9b8b790e031031d8383a
Arun raj <arunraj.17j@gmail.com>
2020-06-14 13:31:04 +0530

    mobile dashboard logo readjut

--- src/components/AboutOP.js
+++ src/components/AboutOP.js
@@ -5,9 +5,7 @@ import {
 } from 'react-router-dom'
 
 class AboutOP extends React.Component {
-    componentDidMount() {
-        window.scrollTo(0, 0)
-    }
+
     render() {
         return (
             <div className='about-op'>
